924eec2256f682a3dfebe3fa0a0c2a114037e4b8
AnonymousPC <haideruet73.055@gmail.com>
2022-12-14 19:27:01 +0500

    Assignment

---
+++ header.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main() {
+cout<<"########################################################"<<endl;
+cout<<"#                                                      #"<<endl;
+cout<<"#        First step to become a programmer             #"<<endl;   
+cout<<"#                                                      #"<<endl;
+cout<<"#            It is really interesting                  #"<<endl;   
+cout<<"#                                                      #"<<endl;
+cout<<"#                                                      #"<<endl;
+cout<<"#                                                      #"<<endl;
+cout<<"#                                                      #"<<endl;
+cout<<"########################################################"<<endl;                                            
+
+
+}
